11e8baf0ccb46a4e7d74eba49564716841fa7bb4
ssongman <ssongmantop@gamil.com>
2019-03-11 23:59:39 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+
+@RestController
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	@RequestMapping(value = "/add/employee", method = RequestMethod.GET)
+	public Employee addEmployee(@RequestParam("name") String name, @RequestParam("empId") String empId) {
+		/*
+		 * localhost:8080/add/employee?empId=1&name=test
+		 */
+		System.out.println("[addEmployee] name["+name+"], empId["+empId+"]");
+		return employeeService.createEmployee(name, empId);
+	}
+
+	@RequestMapping(value = "/remove/employee", method = RequestMethod.GET)
+	public String removeEmployee( @RequestParam("empId") String empId) {
+		/*
+		 * localhost:8080/remove/employee?empId=2
+		 */
+		System.out.println("[removeEmployee] empId["+empId+"]");
+		employeeService.deleteEmployee(empId);
+
+		return "Employee removed";
+	}
+
+}
